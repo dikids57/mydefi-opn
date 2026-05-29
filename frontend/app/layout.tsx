112abda188df5efd94fdfcd654c599efcfce7549
dikids57 <141473918+dikids57@users.noreply.github.com>
2026-05-29 23:47:42 +0800

    Create layout.tsx

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ms">
+      <body>{children}</body>
+    </html>
+  );
+}
